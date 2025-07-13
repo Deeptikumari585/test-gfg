6ea11b7464e7c5d367ed838e87cd907af09686db
Deeptikumari585 <kdeepti185@gmail.com>
2025-07-14 01:46:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern 20/pattern-20.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        
+        //upper half
+        
+    for (int i = 1; i <= n; i++) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Bottom half
+        for (int i = n - 1; i >= 1; i--) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
